2c789d581ca2a718136d8ea270360e88dab992b6
AnWings <joeyjiang592537064@gmail.com>
2023-05-24 12:02:19 -0400

    remove thunks and container for employee update

--- final-frontend-main/src/components/containers/AllEmployeesContainer.js
+++ final-frontend-main/src/components/containers/AllEmployeesContainer.js
@@ -1,18 +1,26 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchAllEmployeesThunk } from "../../store/thunks";
 import { AllEmployeesView } from "../views";
+import { fetchAllEmployeesThunk, deleteEmployeeThunk } from "../../store/thunks";
 
 function AllEmployeesContainer() {
   const allEmployees = useSelector((state) => state.allEmployees);
   const dispatch = useDispatch();
 
-  //replaces componentDidMount
   useEffect(() => {
     dispatch(fetchAllEmployeesThunk());
   }, [dispatch]);
 
-  return <AllEmployeesView allEmployees={allEmployees} />;
+  const handleDeleteEmployee = (employeeId) => {
+    dispatch(deleteEmployeeThunk(employeeId));
+  };
+
+  return (
+    <AllEmployeesView
+      allEmployees={allEmployees}
+      deleteEmployee={handleDeleteEmployee}
+    />
+  );
 }
 
 export default AllEmployeesContainer;
