4eb4e745d0bdb60400b1fd93894853b1136dd910
Carla Sanchez <72993271+CGSanchez-tech@users.noreply.github.com>
2023-05-24 07:44:24 -0400

    Created EditingEmployeeContainer

---
+++ final-frontend-main/src/components/containers/EditEmployeeContainer.js
@@ -0,0 +1,17 @@
+/*
+Submitting the form should: 
+Make a request that causes the employee to be updated in the database  
+Display the updates without needing to refresh the page
+*/
+
+import React from 'react';
+
+function EmployeeForm() {
+    return (
+        <form>
+          {/* Form fields */}
+        </form>
+      ); 
+}
+
+export default EmployeeForm;
