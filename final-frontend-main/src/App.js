fbb84cd174b492ad11f1d0580bb0119c5306f4d1
Carla Sanchez <72993271+CGSanchez-tech@users.noreply.github.com>
2023-05-24 13:06:55 -0400

    small edits

--- final-frontend-main/src/App.js
+++ final-frontend-main/src/App.js
@@ -9,7 +9,7 @@ import {
   TaskContainer,
   AllEmployeesContainer,
   AllTasksContainer,
-  //NewEmployeeContainer,
+  //NewEmployeeContainer, was giving me issues
   NewTaskContainer,
   EditTaskContainer
 } from './components/containers';
@@ -24,6 +24,11 @@ import NewEmployeeContainer from './components/containers/NewEmployeeContainer';
 const App = () => {
   return (
     <div className="App">
+      {/* <nav>
+        <Link to="/"><button>Home</button></Link>
+        <Link to="/employees"><button>Employees</button></Link>
+        <Link to="/tasks"><button>Tasks</button></Link>
+      </nav> */}
       <Switch>
         <Route exact path="/" component={HomePageContainer} />
         <Route exact path="/employees" component={AllEmployeesContainer} />
